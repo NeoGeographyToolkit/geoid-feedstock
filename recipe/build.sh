b8f2bfc73af51067d9ff99b10da2180fad0cf148
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2020-06-20 16:12:46 -0700

    Hack: For OSX use local compiler

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,14 +5,27 @@ set -x
 if [ "$(uname)" = "Darwin" ]; then
     LIB_FLAG='-dynamiclib'
     EXT='.dylib'
+    # Hack, use my own compiler. I could not make the conda fortran work
+    # as it expects libgfortran 4 which is incompatible with LAPACK.
+    FC=/usr/local/bin/gfortran
+    FFLAGS=''
+    LDFLAGS=''
 else
     LIB_FLAG='-shared'
     EXT='.so'
 fi
 
+
 # Build
 ${FC} ${FFLAGS} -fPIC -O3 -c interp_2p5min.f
 ${FC} ${LDFLAGS} ${LIB_FLAG} -o libegm2008${EXT} interp_2p5min.o
+
+if [ "$(uname)" = "Darwin" ]; then
+    for dep in libgfortran.3 libquadmath.0 libgcc_s.1; do 
+        /usr/bin/install_name_tool -change /usr/local/lib/${dep}.dylib \
+            @rpath/${dep}.dylib libegm2008${EXT}
+    done
+fi
 
 # Install
 mkdir -p ${PREFIX}/lib
