9047700231ad352e31ddae788c81fb73a30fb394
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-12-16 19:50:19 -0800

    Update recipe for ISIS 7

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,11 +5,6 @@ set -x
 if [ "$(uname)" = "Darwin" ]; then
     LIB_FLAG='-dynamiclib'
     EXT='.dylib'
-    # Hack, use my own compiler. I could not make the conda fortran work
-    # as it expects libgfortran 4 which is incompatible with LAPACK.
-    FC=/usr/local/bin/gfortran
-    FFLAGS=''
-    LDFLAGS=''
 else
     LIB_FLAG='-shared'
     EXT='.so'
@@ -19,14 +14,6 @@ fi
 # Build
 ${FC} ${FFLAGS} -fPIC -O3 -c interp_2p5min.f
 ${FC} ${LDFLAGS} ${LIB_FLAG} -o libegm2008${EXT} interp_2p5min.o
-
-if [ "$(uname)" = "Darwin" ]; then
-    # Make library paths be relative
-    for dep in libgfortran.3 libquadmath.0 libgcc_s.1; do 
-        /usr/bin/install_name_tool -change /usr/local/lib/${dep}.dylib \
-            @rpath/${dep}.dylib libegm2008${EXT}
-    done
-fi
 
 # Install
 mkdir -p ${PREFIX}/lib
